07609e77301083081a4d34b299d7fe0f708d02c0
Sinaf19 <quentinsurdez@gmail.com>
2023-12-14 16:14:29 +0100

    creating maven project with javalin

---
+++ code/src/main/java/ch/heig/dai/lab/http/API.java
@@ -0,0 +1,13 @@
+package ch.heig.dai.lab.http;
+
+/**
+ * Hello world!
+ *
+ */
+public class API
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
